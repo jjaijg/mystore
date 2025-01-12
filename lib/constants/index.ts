7009588ab33b0f8ca13d5fbe3edca9a41963af4b
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-01-12 10:59:47 +0530

    Added file to maintain app constant

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Mystore";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Ecommerce platform built with Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
