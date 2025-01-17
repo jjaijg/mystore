339f3f1a96fe295c3930e1d30ddb5814ed55a45f
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-01-17 13:47:05 +0530

    Added cart page setup

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.action";
+
+export const metadata: Metadata = {
+  title: "Cart",
+};
+const CartPage = async () => {
+  const cart = await getMyCart();
+
+  return (
+    <div>
+      <CartTable cart={cart} />
+    </div>
+  );
+};
+
+export default CartPage;
