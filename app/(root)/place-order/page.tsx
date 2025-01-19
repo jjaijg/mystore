b49829a3b8c36b1047b9c9456165d2bc5d6641de
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-01-19 22:48:31 +0530

    Added place order page skeleton

---
+++ app/(root)/place-order/page.tsx
@@ -0,0 +1,11 @@
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+const PlaceOrderPage = () => {
+  return (
+    <>
+      <CheckoutSteps current={3} />
+    </>
+  );
+};
+
+export default PlaceOrderPage;
