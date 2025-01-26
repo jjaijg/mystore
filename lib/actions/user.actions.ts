f62841c1c391045bcdfcfa65c5f9e27f28d40814
Jai Ganesh J <jjaiganeshjg95@gmail.com>
2025-01-26 21:02:38 +0530

    added user action to update profile

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -116,3 +116,27 @@ export async function updateUserPaymentMethod(data: PaymentMethod) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// update the user profile
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return { success: true, message: "Profile updated successfully" };
+  } catch (error) {
+    return { success: false, error: formatError(error) };
+  }
+}
